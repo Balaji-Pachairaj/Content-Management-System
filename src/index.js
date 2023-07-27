021357338607a80be3afa53387a84aa33223caf0
Balaji <balajitheblankde@gmail.com>
2023-07-27 13:53:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Router packages
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//Creating router
+const router = createBrowserRouter([
+     {
+          //Home page router => initial rendering page
+          path: "/",
+          element: <p>Home</p>,
+     },
+     {
+          //Sign in => login form for existing user
+          path: "/signin",
+          element: <p className=" text-center">sign in </p>,
+     },
+     {
+          //Sign up => login form for New user
+          path: "/signup",
+          element: <p className=" text-center">sign up </p>,
+     },
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+     <React.StrictMode>
+          <RouterProvider router={router} />
+     </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
